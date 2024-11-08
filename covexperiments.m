976fbf387667f3b509ef188923c6fad2d11df574
DmitryMurashov <dmtr.murashov@gmail.com>
2024-11-08 15:52:35 +0300

    [covexperiments] Ref | Separate dataset generation

--- covexperiments.m
+++ covexperiments.m
@@ -1,31 +1,45 @@
-function [] = main()
+function [] = covexperiments()
+    % Generate dataset
+    tt = [0 5 11 11.5 12 12.5 13 14] * 60;
+    yy = [0 50 40 50 55 30 10 0];
+    [tt, yy, gyy, byy] = generate_data(tt, yy)
+
+    hold on
+    plot(tt, gyy, '--');
+    plot(tt, byy, '--');
+    plot(tt, yy, '-', 'LineWidth', 2);
+    legend("GNSS", "Baro", "Ground truth");
+    hold off
+end
+
+function [tt, yy, gyy, byy] = generate_data(tt, yy)
+    % tt - time
+    % gyy - GNSS data
+    % byy - barometer data
+    % Input:
+    % tt - base points (time)
+    % yy - base points (altitude)
+
     % Trajectory base points
     tt = [0 5 11 11.5 12 12.5 13 14] * 60;
-    yy = [0 50 40 50 55 51 10 0];
+    yy = [0 50 40 50 55 30 10 0];
     [tt, yy] = generate_signal(tt, yy, 0.5);
-    dt = tt(2) - tt(1)
+    dt = tt(2) - tt(1);
 
     % GNSS
     % Add noise
     gsigma = 0.3;
     gyy = yy + randn(size(yy)) * 0.1;
     % Add sag
-    tsag = [0 5 10 15]
-    ysag = [0 -10 5 0]
-    gyy = merge_signal(gyy, tsag, ysag, 2 * 60, dt)
+    tsag = [0 5 8 15];
+    ysag = [0 -10 5 0];
+    gyy = merge_signal(gyy, tsag, ysag, 2 * 60, dt);
 
     % Baro
     % Add noise
     byy = yy + randn(size(yy)) * 0.4;
     % Add drift, 5m
     byy = byy + linspace(0, 5, numel(yy));
-
-    hold on
-    plot(tt, gyy, '--')
-    plot(tt, byy, '--')
-    plot(tt, yy, '-', 'LineWidth', 2)
-    legend("GNSS", "Baro", "Ground truth")
-    hold off
 end
 
 function [tt, yy] = add_noise(tt, yy, var)
