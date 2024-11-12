11bab5e69eba177eaed70f6eee5cbeb294fbe401
DmitryMurashov <dmtr.murashov@gmail.com>
2024-11-12 17:24:32 +0300

    [convert_data_401 ukfside] Impl | UKF implementation (borrowed)

--- convert_data_401.m
+++ convert_data_401.m
@@ -12,14 +12,14 @@ function [ttyybaroalt, ttyygnss, ttyyaccgyro] = convert_data_401(path, sid, gnss
 	% gnssnoisevar: Additional noise for GNSS data, because is rounded to 1m, not usable for real applications
 
 	pressname = [path "session_" sid "_" "Altitude.csv"];
-	ttyybaroalt = csvread(pressname)
+	ttyybaroalt = csvread(pressname);
 
 	gnssname = [path "session_" sid "_" "NavPosition.csv"];
-	ttygnss = csvread(gnssname);
+	ttyygnss = csvread(gnssname);
 
 	accgyroname = [path "session_" sid "_" "RawAccelGyroData.csv"];
 	ttyyaccgyroraw = csvread(accgyroname);
-	ttyaccgyro = process_mpu60xx_accgyro_data(ttyaccgyroraw)
+	ttyyaccgyro = process_mpu60xx_accgyro_data(ttyyaccgyroraw);
 end
 
 function [ttyybaroalt] = process_pressure_data(ttyypressure)
@@ -27,11 +27,11 @@ function [ttyybaroalt] = process_pressure_data(ttyypressure)
 	% ttybaroalt: matrix [TIME, ALTITUDE] (s m)
 	% Converts barometric pressure into barometric altitude. Uses first point as zero reference.
 	groundpressure = mean(ttyypressure(1:3, 2)) % Just a mean from first values
-	ttybaroalt = ttypressure
-	for i in size(ttypressure(1))
-			val = ttypressure(i, 2)
-			tmp = (val / groundpressure) ^ (1 / 5.255)
-			ttybaroalt = 44330 * (1 - tmp)
+	ttyybaroalt = ttyypressure
+	for i = 1:size(ttyypressure(1))
+			val = ttyypressure(i, 2);
+			tmp = (val / groundpressure) ^ (1 / 5.255);
+			ttybaroalt = 44330 * (1 - tmp);
 	end
 end
 
@@ -40,16 +40,16 @@ function [ttyyaccgyro] = process_mpu60xx_accgyro_data(ttyaccgyro_raw)
 	% Acc/Gyro scaling values
 	% ttyaccgyro_raw: matrix [TIME ACCXRAW ACCYRAW ACCZRAW TEMP GYROXRAW GYROYRAW GYROZRAW] (s ADC ADC ADC ~ ADC ADC ADC)
 	% ttyyaccgyro: matrix [TIME ACCX ACCY ACCZ TEMP GYROX GYROY GYROZ] (s m/s2 m/s2 m/s2 ~ deg/s deg/s deg/s)
-	g = 9.80665
-	accscale = 16 % [g]
-	gyroscale = 2000 % [deg/s]
-	resolution = 2^16 - 1 % 16 bit ADC
+	g = 9.80665;
+	accscale = 16; % [g]
+	gyroscale = 2000; % [deg/s]
+	resolution = 2^16 - 1; % 16 bit ADC
 
-	ttyyaccgyro = ttyaccgyro_raw
-	ttyaccgyro = ttyaccgyro_raw(:, 2) / resolution * accscale * g
-	ttyaccgyro = ttyaccgyro_raw(:, 3) / resolution * accscale * g
-	ttyaccgyro = ttyaccgyro_raw(:, 4) / resolution * accscale * g
-	ttyaccgyro = ttyaccgyro_raw(:, 6) / resolution * gyroscale
-	ttyaccgyro = ttyaccgyro_raw(:, 7) / resolution * gyroscale
-	ttyaccgyro = ttyaccgyro_raw(:, 8) / resolution * gyroscale
+	ttyyaccgyro = ttyaccgyro_raw;
+	ttyyaccgyro = ttyaccgyro_raw(:, 2) / resolution * accscale * g;
+	ttyyaccgyro = ttyaccgyro_raw(:, 3) / resolution * accscale * g;
+	ttyyaccgyro = ttyaccgyro_raw(:, 4) / resolution * accscale * g;
+	ttyyaccgyro = ttyaccgyro_raw(:, 6) / resolution * gyroscale;
+	ttyyaccgyro = ttyaccgyro_raw(:, 7) / resolution * gyroscale;
+	ttyyaccgyro = ttyaccgyro_raw(:, 8) / resolution * gyroscale;
 end
